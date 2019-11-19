dc8c0d9132b01f9e5e7f0a3d384aaa6a6d474c8c
Mago Gallardo <gallardo.dominguez.emmanuel@gmail.com>
2019-11-19 11:55:33 -0600

    Added renders

--- Blog/core/views.py
+++ Blog/core/views.py
@@ -2,5 +2,14 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.shortcuts import HttpResponse
 
+def home(request):
+	return render(request, "core/home.html")
+def contact(request):
+	return HttpResponse("<h1>Contact<h1>")
+def cd(request):
+	return HttpResponse("<h1>cd<h1>")
+def portfolio(request):
+	return HttpResponse("<h1>portfolio<h1>")
 # Create your views here.
